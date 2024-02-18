7a139cb3f672b27d3ed97dfee0b7d61a7b245a4e
Mher Hovhannisyan <mh3r99@gmail.com>
2024-02-18 18:40:15 +0400

    add styles, fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import { AppProps } from 'next/app';
+import '../styles/globals.css';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Next Top App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
