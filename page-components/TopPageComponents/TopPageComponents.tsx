d35b20e41f1427381763db9aafd4038430a20d42
Mher Hovhannisyan <mh3r99@gmail.com>
2024-09-18 18:33:59 +0400

    seoText

--- page-components/TopPageComponents/TopPageComponents.tsx
+++ page-components/TopPageComponents/TopPageComponents.tsx
@@ -1,12 +1,9 @@
-import React from "react";
-import {
-  TopLevelCategory,
-  TopPageModel,
-} from "../../interfaces/page.interface";
-import { ProductModel } from "../../interfaces/product.interface";
-import styles from "./TopPageComponents.module.css";
-import { Htag, Tag, HhData, Paragraph } from "../../components";
-import { Advantages } from "../../components/Advantages/Advantages";
+import React from 'react';
+import { TopLevelCategory, TopPageModel } from '../../interfaces/page.interface';
+import { ProductModel } from '../../interfaces/product.interface';
+import styles from './TopPageComponents.module.css';
+import { Htag, Tag, HhData, Paragraph } from '../../components';
+import { Advantages } from '../../components/Advantages/Advantages';
 
 interface ITopPageComponentsProps extends Record<string, unknown> {
   firstCategory: TopLevelCategory;
@@ -30,28 +27,34 @@ const TopPageComponents: React.FC<ITopPageComponentsProps> = ({
         )}
         <span>Сортировка</span>
       </div>
-      <div>
-        {products && products.map((p) => <div key={p._id}>{p.title}</div>)}
-      </div>
+      <div>{products && products.map((p) => <div key={p._id}>{p.title}</div>)}</div>
       <div className={styles.hhTitle}>
         <Htag tag="h2">Вакансии - {page.category}</Htag>
         <Tag color="red" size="m">
           hh.ru
         </Tag>
       </div>
-      {firstCategory == TopLevelCategory.Courses && page.hh && (
-        <HhData {...page.hh} />
-      )}
+      {firstCategory == TopLevelCategory.Courses && page.hh && <HhData {...page.hh} />}
       {page.advantages && page.advantages.length > 0 && (
         <>
           <Htag tag="h2">Преимущества</Htag>
           <Advantages advantages={page.advantages} />
         </>
       )}
-      {page.seoText && <Paragraph>{page.seoText}</Paragraph> }
+      {page.seoText && (
+        <div
+          className={styles.seo}
+          dangerouslySetInnerHTML={{
+            __html: page.seoText,
+          }}
+        />
+      )}
       <Htag tag="h2">Получаемые навыки</Htag>
-      {page.tags.map(tag=> <Tag key={tag} color="primary">{tag}</Tag> )}
-      
+      {page.tags.map((tag) => (
+        <Tag key={tag} color="primary">
+          {tag}
+        </Tag>
+      ))}
     </div>
   );
 };
